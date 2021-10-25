a679b83bec0c278a8443f3c4ed183f51e4605b72
fredericstlaurent <frederic.st-laurent.1@ens.etsmtl.ca>
2021-10-25 11:13:42 -0400

    e

--- dist/docs/api_project.js
+++ dist/docs/api_project.js
@@ -8,7 +8,7 @@ define({
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2021-10-19T14:18:05.862Z",
+    "time": "2021-10-25T13:22:19.146Z",
     "url": "https://apidocjs.com",
     "version": "0.29.0"
   }
